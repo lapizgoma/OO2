6ef857499484f136d9f339b394b5ee7c4e1bb9fe
kevinvittor <kevinvittor579@gmail.com>
2025-04-25 18:59:13 -0300

    add metodo toDto

--- src/model/Empleado.java
+++ src/model/Empleado.java
@@ -1,29 +1,71 @@
 package model;
 
+import java.time.LocalDateTime;
+
+import dto.EmpleadoDTO;
+import enums.EnumRole;
+
 public class Empleado extends Usuario {
 	
-	private String legajo;
+	private LocalDateTime createdAt;
+	private LocalDateTime updateAt;
+	private EnumRole role;
 	
 	public Empleado() {
+		super();
+	}
+	public Empleado(String email, String apellido, String nombre, String password, boolean deleted) {
+		super(email, apellido, nombre, password, deleted);
 	}
 	
-	public Empleado(String email, String apellido, String nombre, String password,String legajo) {
-		super(email, apellido, nombre, password);
-		this.legajo = legajo;
+	public Empleado(String email, String apellido, String nombre, String password, boolean deleted, EnumRole role) {
+		super(email, apellido, nombre, password, deleted);
+		this.createdAt = LocalDateTime.now();
+		this.role = role;
 	}
 	
-	public String getLegajo() {
-		return legajo;
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
 	}
-
-	public void setLegajo(String legajo) {
-		this.legajo = legajo;
+	
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
 	}
+	
+	public EnumRole getRole() {
+		return role;
+	}
+	
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+	
+	public void setRole(EnumRole role) {
+		this.role = role;
+	}
+	
+	public EmpleadoDTO empleadoToDto() {
+		EmpleadoDTO empleadoDto = new EmpleadoDTO();
+		empleadoDto.setId(this.id);
+		empleadoDto.setNombre(this.nombre);
+		empleadoDto.setRole(this.role.toString());
+		return empleadoDto;
+	}
+	
 	
 	@Override
 	public String toString() {
-		return super.toString();
+		return "Empleado [createdAt=" + createdAt + ", updateAt=" + updateAt + ", role=" + role + ", id=" + id
+				+ ", email=" + email + ", apellido=" + apellido + ", nombre=" + nombre + ", password=" + password + "]";
 	}
-
+	
+	
+	
+	
+	
 
 }
