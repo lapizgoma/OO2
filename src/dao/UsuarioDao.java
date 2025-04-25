efb3fb15f3ea4282bdd43af607a7f68aa4fad5d9
kevinvittor <kevinvittor579@gmail.com>
2025-04-25 18:54:58 -0300

    add CU Kevin-Vittor

--- src/dao/UsuarioDao.java
+++ src/dao/UsuarioDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.hibernate.HibernateException;
 
+import model.Cliente;
 import model.Empleado;
 import model.Usuario;
 
@@ -60,4 +61,45 @@ public class UsuarioDao extends Dao<Usuario> {
 		}
 	}
 	
+	public Cliente traerCliente(Long id) {
+		Cliente cliente = null;
+		try {
+			iniciaOperacion();
+			cliente = session.createQuery("from Cliente c where id = :id",Cliente.class)
+					.setParameter("id", id)
+					.getSingleResult();
+		}finally {
+			session.close();
+		}
+		return cliente;
+	}
+	
+	public List<Cliente> traerClientes(){
+		List<Cliente> clientes = new ArrayList<Cliente>();
+		try {
+			iniciaOperacion();
+			clientes = session.createQuery("from Cliente c where",Cliente.class).getResultList();
+		}finally {
+			session.close();
+		}
+		return clientes;
+	}
+	
+	/*
+	 * CU Kevin Vittor
+	 * Aclaracion: crear Usuario ya realizado gracias al Dao.java
+	 */
+	public void agregarCliente(Cliente cliente) {
+		try {
+			iniciaOperacion();
+			session.save(cliente);
+			tx.commit();
+		}catch (HibernateException e) {
+			manejaExcepcion(e);
+			throw e;
+		}finally {
+			session.close();
+		}
+	}
+
 }
