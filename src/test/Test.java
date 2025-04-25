8e4f790ee6749d6229ec796ad35f024438eb1b7d
kevinvittor <kevinvittor579@gmail.com>
2025-04-25 19:02:03 -0300

    modify clase test con las nuevas actualizaciones

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,13 +1,16 @@
 package test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
+import enums.EnumRole;
 import enums.EstadosTicket;
+import model.Cliente;
 import model.Empleado;
 import model.Mensaje;
 import model.Ticket;
@@ -17,7 +20,7 @@ import negocio.TicketABM;
 import negocio.UsuarioABM;
 
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		UsuarioABM usuario = new UsuarioABM();
 		MensajeABM mensajeABM = new MensajeABM();
 		TicketABM ticketABM = new TicketABM();
@@ -29,24 +32,34 @@ public class Test {
 		Set<Mensaje> mensajes3 = new HashSet<Mensaje>();
 		Random rand = new Random();
 		
-		usuario.agregarEmpleado(new Empleado("kevin@hotmail.com","Vittor","Kevin","222222","10101010"));
-		usuario.agregarEmpleado(new Empleado("robert@hotmail.com","Asafg","Roberto","222222","20202020"));
+		usuario.agregarEmpleado(new Empleado("kevin@hotmail.com","Vittor","Kevin","222222",false,EnumRole.ADMIN));
+		usuario.agregarEmpleado(new Empleado("robert@hotmail.com","Asafg","Roberto","34343",false,EnumRole.EMPLEADO));
 		List<Empleado> empleados = usuario.traerEmpleado();
 		
-		usuarios.add(new Usuario("jose@hotmail.com", "Rodriguez", "Jose","10000000"));
-		usuarios.add(new Usuario("hernan@hotmail.com", "Perez", "Hernan","111111"));
-		usuarios.add(new Usuario("leonel@hotmail.com", "Aguirre", "Leonel","rodkkkk"));
+		usuarios.add(new Usuario("jose@hotmail.com", "Rodriguez", "Jose","10000000",true));
+		usuarios.add(new Cliente("hernan@hotmail.com", "Perez", "Hernan","111111",false,"12354577", "Jose c paz"));
+		usuarios.add(new Cliente("leonel@hotmail.com", "Aguirre", "Leonel","rodkkkk",false,"12345678","Bernal"));
 		
-		mensajes.add(new Mensaje("Tuve problemas con el ticket", LocalDate.now(), usuarios.get(0)));
-		mensajes.add(new Mensaje("No estas resolviendo mi duda", LocalDate.now(), usuarios.get(0)));
-		mensajes.add(new Mensaje("Entendido", LocalDate.now(), usuarios.get(0)));
-		mensajes.add(new Mensaje("Muchas gracias !!", LocalDate.now(), usuarios.get(0)));
+
+		Thread.sleep(500);
+		mensajes.add(new Mensaje("Tuve problemas con el ticket", LocalDateTime.now(), usuarios.get(0)));
+		Thread.sleep(500);
+		mensajes.add(new Mensaje("No estas resolviendo mi duda", LocalDateTime.now(), usuarios.get(0)));
+		Thread.sleep(500);
+		mensajes.add(new Mensaje("Entendido", LocalDateTime.now(), usuarios.get(0)));
+		Thread.sleep(500);
+		mensajes.add(new Mensaje("Muchas gracias !!", LocalDateTime.now(), usuarios.get(0)));
 		
-		mensajes2.add(new Mensaje("Tuve problemas con el ticket", LocalDate.now(), usuarios.get(1)));
-		mensajes2.add(new Mensaje("Ok esperare", LocalDate.now(), usuarios.get(1)));
 		
-		mensajes3.add(new Mensaje("Tengo problemas con el producto", LocalDate.now(), usuarios.get(2)));
-		mensajes3.add(new Mensaje("Tienen que gestionar mi ticket?", LocalDate.now(), usuarios.get(2)));
+		Thread.sleep(500);
+		mensajes2.add(new Mensaje("Tuve problemas con el ticket", LocalDateTime.now(), usuarios.get(1)));
+		Thread.sleep(500);
+		mensajes2.add(new Mensaje("Ok esperare", LocalDateTime.now(), usuarios.get(1)));
+		
+		Thread.sleep(500);
+		mensajes3.add(new Mensaje("Tengo problemas con el producto", LocalDateTime.now(), usuarios.get(2)));
+		Thread.sleep(500);
+		mensajes3.add(new Mensaje("Tienen que gestionar mi ticket?", LocalDateTime.now(), usuarios.get(2)));
 		
 		tickets.add(new Ticket(usuarios.get(0),"Incovenientes",EstadosTicket.CERRADO));
 		tickets.add(new Ticket(usuarios.get(1),"Incovenientes",EstadosTicket.ATENDIDO));
@@ -73,7 +86,7 @@ public class Test {
 	public static void persistenciaMensaje(Set<Mensaje> mensajes, MensajeABM mensajeABM,Ticket ticket) {
 		for(Mensaje m: mensajes) {
 		    ticket.agregarMensaje(m);
-		    mensajeABM.agregar(m);
+			mensajeABM.agregar(m);
 		}
 	}
 }
