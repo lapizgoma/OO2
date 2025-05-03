e8619f759d98a404d71dc51100ca259b2102e927
lapizgoma <estudiosnomas@gmail.com>
2025-05-03 11:26:28 -0300

    SpringBoot con Maven e Hibernate

---
+++ src/main/java/oo2/grupo19/SistemaTickets/SistemaTicketsApplication.java
@@ -0,0 +1,13 @@
+package oo2.grupo19.SistemaTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTicketsApplication.class, args);
+	}
+
+}
