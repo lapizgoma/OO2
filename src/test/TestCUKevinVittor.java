2a11334098db57cb764a59f9b87a9054c70d3c5b
kevinvittor <kevinvittor579@gmail.com>
2025-05-04 18:24:29 -0300

    Test CU

--- src/test/TestCUKevinVittor.java
+++ src/test/TestCUKevinVittor.java
@@ -3,13 +3,34 @@ package test;
 import enums.EstadosTicket;
 import model.Cliente;
 import model.Ticket;
+import model.Usuario;
 import negocio.TicketABM;
+import negocio.UsuarioABM;
 
 public class TestCUKevinVittor {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		
+		// CU 1. Crear usuario
+		
+		UsuarioABM usuarioABM = new UsuarioABM();
+		Usuario usuario = new Usuario("sergio@hotmail.com", "figueroa", "sergio", "111111", false);
+		usuarioABM.agregar(usuario);
+		Thread.sleep(5000);
+		System.out.println("CU 1. Completado");
+		
+		
+		// CU 2. Crear cuenta
+		
+		Cliente cliente = usuario.crearCuenta("54325642", "Victor Hugo");
+		usuarioABM.agregar(cliente);
+		Thread.sleep(5000);
+		System.out.println("CU 2. Completado");
+		
+		// CU 3. El Cliente genera un ticket
 		TicketABM ticket = new TicketABM();
-		System.out.println("HOLA");
-		Ticket ticketNuevo = new Ticket(new Cliente("jaime@hotmail.com", "lorez", "jaime", "saracatunga", false,"333333","av Corrientes"), "Consultaas2", EstadosTicket.PENDIENTE);
-		ticket.agregarUsuarioTicket(ticketNuevo);
+		Ticket ticketNuevo = new Ticket(cliente, "Problemas tecnicos", EstadosTicket.PENDIENTE);
+		ticket.agregar(ticketNuevo);
+		Thread.sleep(5000);
+		System.out.println("CU 3. Completado");
 	}
 }
