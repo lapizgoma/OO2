cd464bcb16fdf597a55919dddccbe0a2b42c58e0
lapizgoma <estudiosnomas@gmail.com>
2025-05-05 12:02:27 -0300

    Implementada actualización usuario

--- src/negocio/UsuarioABM.java
+++ src/negocio/UsuarioABM.java
@@ -8,7 +8,7 @@ import model.Empleado;
 import model.Usuario;
 
 public class UsuarioABM implements INegocio<Usuario> {
-	
+
 	private UsuarioDao usuarioDao = new UsuarioDao();
 
 	@Override
@@ -32,18 +32,27 @@ public class UsuarioABM implements INegocio<Usuario> {
 	}
 
 	@Override
-	public void eliminar(Usuario objeto) {
-		usuarioDao.eliminar(objeto);
+	public void eliminar(Usuario u) {
+		if (u != null) {
+			// al traerlo desde la BD, validamos de que exista.
+			// al usar el dato traido, garantizamos que solo se actualice el estado "deleted".
+			Usuario usuario_db = usuarioDao.traer(u.getId());
+			if (usuario_db != null) {
+				usuario_db.setDeleted(true);
+				usuarioDao.actualizar(usuario_db);
+			}
+			u.setDeleted (true);
+		}
 	}
-	
+
 	public Empleado traerEmpleado(String email) {
 		return usuarioDao.traerEmpleado(email);
 	}
-	
+
 	public List<Empleado> traerEmpleado() {
 		return usuarioDao.traerEmpleados();
 	}
-	
+
 	public void agregarEmpleado(Empleado empleado) {
 		usuarioDao.agregarEmpleado(empleado);
 	}
