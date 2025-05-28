05c98da57839faf190b97997b72425bb099cadf5
kevinVtt <kevinvittor579@gmail.com>
2025-05-28 16:38:01 -0300

    add rutas para las vistas

---
+++ src/main/java/oo2/grupo19/SistemaTickets/helpers/ViewRouteHelper.java
@@ -0,0 +1,11 @@
+package oo2.grupo19.SistemaTickets.helpers;
+
+public class ViewRouteHelper {
+    
+    public static final String LOGIN = "formsCredenciales/login";
+    public static final String REGISTER = "formsCredenciales/register";
+    public static final String INDEX = "/home";
+    public static final String FORM_TICKET = "/ticket/formTicket";
+    public static final String ERROR_404 = "/errors/404";
+
+}
