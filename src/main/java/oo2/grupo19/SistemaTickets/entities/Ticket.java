a06d87895abf1c09076876215e482d3ab96ce33a
kevinVtt <kevinvittor579@gmail.com>
2025-05-29 19:05:32 -0300

    update relaciones a @ManyToOne en los estados

--- src/main/java/oo2/grupo19/SistemaTickets/entities/Ticket.java
+++ src/main/java/oo2/grupo19/SistemaTickets/entities/Ticket.java
@@ -17,7 +17,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Getter;
