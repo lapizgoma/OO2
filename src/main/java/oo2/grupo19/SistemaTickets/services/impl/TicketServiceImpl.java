210252028f2a3adde64f413319a0a7db8ada880d
lapizgoma <estudiosnomas@gmail.com>
2025-06-07 00:22:15 -0300

    Implementado getTicketParaCliente y getTicketparaEmpleado en Service

--- src/main/java/oo2/grupo19/SistemaTickets/services/impl/TicketServiceImpl.java
+++ src/main/java/oo2/grupo19/SistemaTickets/services/impl/TicketServiceImpl.java
@@ -1,30 +1,50 @@
 package oo2.grupo19.SistemaTickets.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.management.RuntimeErrorException;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import oo2.grupo19.SistemaTickets.dto.IntervencionDTO;
+import oo2.grupo19.SistemaTickets.dto.TicketClientDTO;
 import oo2.grupo19.SistemaTickets.dto.TicketDTO;
+import oo2.grupo19.SistemaTickets.dto.TicketEmployeeDTO;
+import oo2.grupo19.SistemaTickets.dto.UsuarioDTO;
+import oo2.grupo19.SistemaTickets.dto.mappers.TicketMapper;
+import oo2.grupo19.SistemaTickets.entities.Cliente;
+import oo2.grupo19.SistemaTickets.entities.Empleado;
+import oo2.grupo19.SistemaTickets.entities.Intervencion;
 import oo2.grupo19.SistemaTickets.entities.Ticket;
 import oo2.grupo19.SistemaTickets.entities.Usuario;
+import oo2.grupo19.SistemaTickets.entities.estados.EstadoTicket;
+import oo2.grupo19.SistemaTickets.exceptions.NotAuthorizedException;
+import oo2.grupo19.SistemaTickets.exceptions.TicketNotFoundException;
+import oo2.grupo19.SistemaTickets.exceptions.UserNotFounException;
+import oo2.grupo19.SistemaTickets.repositories.ICliente;
+import oo2.grupo19.SistemaTickets.repositories.IEmpleado;
 import oo2.grupo19.SistemaTickets.repositories.ITicket;
 import oo2.grupo19.SistemaTickets.repositories.IUsuario;
 import oo2.grupo19.SistemaTickets.services.IService;
+
 
 @Service
 @Primary
 public class TicketServiceImpl implements IService<Ticket>{
     
     private final ITicket ticketRepository;
-    private final IUsuario clienteRepository;
+    private final IUsuario usuarioRepository;
+    private final ICliente clienteRepository;
+    private final IEmpleado empleadoRepository;
 
-    public TicketServiceImpl(ITicket ticketRepository, IUsuario clienteRepository) {
+    public TicketServiceImpl(ITicket ticketRepository, IUsuario usuarioRepository, ICliente clienteRepository, IEmpleado empleadoRepository) {
         this.ticketRepository = ticketRepository;
+        this.usuarioRepository = usuarioRepository;
         this.clienteRepository = clienteRepository;
+        this.empleadoRepository= empleadoRepository;
     }
 
     @Override
@@ -61,7 +81,7 @@ public class TicketServiceImpl implements IService<Ticket>{
     @Transactional
     public void save(Ticket object) {
         try{
-            Usuario userDb = clienteRepository.findById(object.getCreadoPor().getId()).orElseThrow();
+            Usuario userDb = usuarioRepository.findById(object.getCreadoPor().getId()).orElseThrow();
             object.setCreadoPor(userDb);
             ticketRepository.save(object);
         }catch(Error e){
@@ -98,6 +118,34 @@ public class TicketServiceImpl implements IService<Ticket>{
 
     return dto;
 }
+    @Transactional (readOnly = true)
+    public List<Ticket> traerPorEstados (long idEstado) {
+        return ticketRepository.traerPorEstado(idEstado);
+    }
 
     
+
+    public TicketClientDTO getTicketParaCliente(Long ticketId, String clienteEmail) {
+        Ticket ticketEntity = ticketRepository.findById  (ticketId).orElseThrow (() -> new TicketNotFoundException("No pudimos encontrar el Ticket que buscás :/"));
+        Cliente clienteEntity = clienteRepository.findByContactoEmail(clienteEmail)
+        .orElseThrow(() -> new UserNotFounException("Cliente no encontrado :/"));
+
+        System.out.println("Ticket Entity: " + ticketEntity);
+        System.out.println("Cliente Entity: " + clienteEntity);
+        
+        if (!ticketEntity.usuarioPertenece(clienteEntity)) {
+            //  mensaje para despistar
+            throw new NotAuthorizedException("No pudimos encontrar el Ticket que buscás :/");
+        }
+
+        return TicketMapper.mapToTicketClientDto(ticketEntity);
+    }
+
+    public TicketEmployeeDTO getTicketparaEmpleado (Long ticketId, String empleadoEmail) {
+        Ticket ticketEntity = ticketRepository.findById  (ticketId).orElseThrow (() -> new TicketNotFoundException("No pudimos encontrar el Ticket que buscás :/"));
+        Empleado empleadoEntity = empleadoRepository.findByContactoEmail(empleadoEmail)
+        .orElseThrow(() -> new UserNotFounException("Empleado no encontrado :/"));
+
+        return TicketMapper.mapToTicketEmployeeDto(ticketEntity);
+    }
 }
