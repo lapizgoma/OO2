3b8d7ad0262d2ff139960a4f1afafd28df22683d
kevinVtt <kevinvittor579@gmail.com>
2025-06-29 16:42:59 -0300

    add crear-ticket

--- src/main/java/oo2/grupo19/SistemaTickets/controllers/apirest/RestControllerTest.java
+++ src/main/java/oo2/grupo19/SistemaTickets/controllers/apirest/RestControllerTest.java
@@ -3,17 +3,16 @@ package oo2.grupo19.SistemaTickets.controllers.apirest;
 import lombok.extern.log4j.Log4j2;
 import oo2.grupo19.SistemaTickets.controllers.apirest.dto.personaJuridica.PersonaJuridicaRequestDTO;
 import oo2.grupo19.SistemaTickets.dto.EmpleadoDTO;
+import oo2.grupo19.SistemaTickets.dto.EstadoTicketDTO;
+import oo2.grupo19.SistemaTickets.dto.TicketDTO;
 import oo2.grupo19.SistemaTickets.dto.UsuarioDTO;
 import oo2.grupo19.SistemaTickets.dto.api.LoginRequestDTO;
 import oo2.grupo19.SistemaTickets.entities.Usuario;
 import oo2.grupo19.SistemaTickets.exceptions.StatusCustomExceptions.NotFoundException;
-import oo2.grupo19.SistemaTickets.services.IClienteService;
-import oo2.grupo19.SistemaTickets.services.IEmpleadoService;
-import oo2.grupo19.SistemaTickets.services.IPersonaJuridicaService;
-import oo2.grupo19.SistemaTickets.services.ITicketService;
-import oo2.grupo19.SistemaTickets.services.IUsuarioService;
+import oo2.grupo19.SistemaTickets.services.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,17 +30,20 @@ public class RestControllerTest {
     private final IEmpleadoService empleadoService;
     private final ITicketService ticketService;
     private final IPersonaJuridicaService personaJuridicaService;
+    private final IEstadoTicketService estadoTicketService;
 
     public RestControllerTest(IClienteService clienteService,
                             IUsuarioService usuarioService,
                             IEmpleadoService empleadoService,
                             ITicketService ticketService,
-                            IPersonaJuridicaService personaJuridicaService) {
+                            IPersonaJuridicaService personaJuridicaService,
+                              IEstadoTicketService estadoTicketService) {
         this.clienteService = clienteService;
         this.usuarioService = usuarioService;
         this.empleadoService = empleadoService;
         this.ticketService = ticketService;
         this.personaJuridicaService = personaJuridicaService;
+        this.estadoTicketService = estadoTicketService;
     }
 
     @PostMapping("/login")
@@ -86,6 +88,25 @@ public class RestControllerTest {
         }
     }
 
+    @PostMapping("/crear-ticket")
+    public ResponseEntity<?> crearTicket(@Valid @RequestBody TicketDTO ticketDTO){
+        try{
+            UsuarioDTO usuarioBd = usuarioService.findByEmail(ticketDTO.getClienteEmail());
+            if(usuarioBd != null){
+                EstadoTicketDTO estado = estadoTicketService.findById(1L);
+                ticketDTO.setEstado(estado);
+                ticketService.save(ticketDTO);
+                log.info("Ticket creado con exito!");
+                TicketDTO ticketDtoBd = ticketService.findUltimoPorEmailYAsunto(ticketDTO.getClienteEmail(),ticketDTO.getAsunto());
+                return ResponseEntity.status(HttpStatus.OK).body(ticketDtoBd);
+            }else{
+                return ResponseEntity.badRequest().body("El cliente con ese email no existe!");
+            }
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Error al crear un ticket" + e.getMessage());
+        }
+    }
+
     @GetMapping("/employees")
     public ResponseEntity<?> obtenerEmpleados() {
         try {
@@ -107,7 +128,7 @@ public class RestControllerTest {
     }
 
     @GetMapping("/grupo")
-    public ResponseEntity<?> getPersonaJuridica(@Valid @ModelAttribute PersonaJuridicaRequestDTO param)
+    public ResponseEntity<?> getPersonaJuridica(@Valid @RequestBody PersonaJuridicaRequestDTO param)
     {
         try
         {
