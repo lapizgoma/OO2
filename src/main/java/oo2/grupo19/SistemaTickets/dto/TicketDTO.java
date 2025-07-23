b187bec6a99885a0e5f5274c6b7821097955eafc
kevinVtt <kevinvittor579@gmail.com>
2025-07-23 20:42:31 -0300

    add @NotBlank a los atributos de tipo string

--- src/main/java/oo2/grupo19/SistemaTickets/dto/TicketDTO.java
+++ src/main/java/oo2/grupo19/SistemaTickets/dto/TicketDTO.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -21,14 +22,17 @@ import lombok.ToString;
 public class TicketDTO {
 	@Schema(hidden = true)
 	private Long id;
-	private String asunto;
-	private String detalle;
+    @NotBlank(message = "no debe estar vacío")
+    private String detalle;
+    @NotBlank(message = "no debe estar vacío")
+    private String asunto;
 	@Schema(hidden = true)
 	private String fechaHoraCreado;
 	@Schema(hidden = true)
 	private Set<IntervencionDTO> intervenciones = new TreeSet<>(Comparator.comparing(IntervencionDTO::getId)).reversed();
 	@Schema(hidden = true)
 	private EstadoTicketDTO estado;
+    @NotBlank(message = "no debe estar vacío")
 	private String clienteEmail;
 
 	@Schema(hidden = true)
